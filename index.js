65f4547c21b58b664b33b8b315356a6a792f2358
afhallahan <allyson.hallahan@gmail.com>
2023-10-12 15:29:36 -0600

    add express and morgan to index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+/*!
+ * express
+ * Copyright(c) 2009-2013 TJ Holowaychuk
+ * Copyright(c) 2013 Roman Shtylman
+ * Copyright(c) 2014-2015 Douglas Christopher Wilson
+ * MIT Licensed
+ */
 const express = require('express');
  morgan = require('morgan');
 
 const app = express();
+const port = 8080;
 
 //Use Morgan middleware to log requests
 app.use(morgan('combined'));
